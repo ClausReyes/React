5d414c8cb2647769d89c4006a05cff0ae5c631fa
Claudia Reyes <clausreyesbravo@gmail.com>
2023-11-10 16:03:00 -0600

    hasta leccion 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import ProfileCard from "./components/ProfileCard";
+import AlexaImg from "./img/alexa.png";
+import SiriImg from "./img/siri.png";
+import CortanaImg from "./img/cortana.png";
+import "bulma/css/bulma.css"
+import NavBar from "./components/NavBar/NavBar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+    return(
+
+        <div>
+            <section className="hero is-warning">
+                <div className="hero-body">
+            <h1 className="title">Light&Color</h1>
+            <h2 className="subtitle">Arreglos de globos para tus eventos</h2>
+            </div>
+            </section>
+
+            <div className="container">
+                <div className="columns">
+                    <div className="column is-4">
+                    <ProfileCard titulo= "Alexa" arroba= "@alexa22" img= {AlexaImg}/>
+                    </div>
+                    <div className="column is-4">
+                    <ProfileCard titulo= "Portana" arroba= "@portana22" img={CortanaImg}/>
+                    </div>
+                    <div className="column is-4">
+                    <ProfileCard titulo= "Siri" arroba= "@siri22" img={SiriImg}/>
+                    </div>
+
+                </div>
+            
+
+            </div>
+        </div>
+    )
 }
 
 export default App;
+
+
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+const NavBar = ()=>{
+return(
+    <nav>
+        <h3>Light and Color</h3>
+        <div>
+        <button>Arcos</button>
+        <button>Aros</button>
+        <button>Torres</button>
+        <button>Renta de mobiliario</button>
+
+        </div>
+    </nav>
+)
+}
+
+export default NavBar;
