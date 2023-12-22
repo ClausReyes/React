1ea0e086cf3307dd6d326acd87850ac22e03e512
Claudia Reyes <clausreyesbravo@gmail.com>
2023-12-21 23:57:28 -0600

    cart widget con error

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,8 +5,8 @@ import CartItem from "../CartItem/CartItem"
 import { Link } from "react-router-dom"
 
 const Cart = () => {
-    const {cart, clearCart, totalQuantity, total}= useContext(CartContext)
-    if(totalQuantity === 0){
+    const {cart, clearCart, totalQuantity, total} = useContext(CartContext)
+    if (totalQuantity === 0) {
         return(
             <div>
                 <h1>No hay items en el carrito</h1>
@@ -14,9 +14,9 @@ const Cart = () => {
             </div>
         )
     }
-    return(
+    return (
         <div>
-         {cart.map(p => <CartItem key= {p.id}{...p}/>)}
+         { cart.map(p => <CartItem key= {p.id}{...p}/>)}
         <h3>Total: ${total}</h3>
         <button onClick={() => clearCart()} className="Button">Limpiar carrito</button>
         <Link to="/checkout" className="Option">Checkout</Link>
